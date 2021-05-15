e13d83ea9456ea1320c30eb3d67d8f1c8aaf94f7
No object's pointer <35094942+MrWater98@users.noreply.github.com>
2021-05-15 16:27:17 +0800

    Implement the style of repo icon

--- src/components/GithubRepoPanel/index.tsx
+++ src/components/GithubRepoPanel/index.tsx
@@ -266,12 +266,51 @@ export default function GithubRepoPanel({
           hideInput = true
         }}
       >
-        <StyledTokenName className="token-symbol-container" active={Boolean(currency && currency.symbol)}>
-          {repoName}
-        </StyledTokenName>
-        <StyledTokenName className="token-symbol-container" active={Boolean(currency && currency.symbol)}>
-          Owner or Committer
-        </StyledTokenName>
+        <InputRow style={hideInput ? { padding: '0', borderRadius: '8px' } : {}} selected={!onCurrencySelect}>
+          <CurrencySelect
+            selected={!!currency}
+            hideInput={hideInput}
+            className="open-currency-select-button"
+            onClick={() => {
+              if (onCurrencySelect) {
+                setModalOpen(false)
+              }
+            }}
+          >
+            <Aligner>
+              <RowFixed>
+                {pair ? (
+                  <span style={{ marginRight: '0.5rem' }}>
+                    <DoubleCurrencyLogo currency0={pair.token0} currency1={pair.token1} size={24} margin={true} />
+                  </span>
+                ) : currency ? (
+                  <CurrencyLogo style={{ marginRight: '0.5rem' }} currency={currency} size={'24px'} />
+                ) : null}
+                {pair ? (
+                  <StyledTokenName className="pair-name-container">
+                    {pair?.token0.symbol}:{pair?.token1.symbol}
+                  </StyledTokenName>
+                ) : (
+                  <StyledTokenName className="token-symbol-container" active={Boolean(currency && currency.symbol)}>
+                    {repoName}
+                  </StyledTokenName>
+                )}
+              </RowFixed>
+            </Aligner>
+          </CurrencySelect>
+          {!hideInput && (
+            <>
+              <NumericalInput
+                className="token-amount-input"
+                value={value}
+                placeholder={'Owner'}
+                onUserInput={(val) => {
+                  onUserInput(val)
+                }}
+              />
+            </>
+          )}
+        </InputRow>
         <ButtonLight
           onClick={() => {
             if (!activated) handleContractCreate(repoName, githubInfo)
